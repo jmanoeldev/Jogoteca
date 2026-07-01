f5f1e8ec13ec5010a1f24d413aa5d33a839cc1ec
José Manoel <jose1manoel2silva@gmail.com>
2026-06-30 22:44:26 -0300

    aulas sobre sessoes e mensagens flash com flask

--- jogoteca/jogoteca.py
+++ jogoteca/jogoteca.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session, flash
 
 class Jogo:
     def __init__(self, nome, categoria, console):
@@ -7,6 +7,7 @@ class Jogo:
         self.console = console
 
 app = Flask(__name__)
+app.secret_key = 'manoel'
 
 jogo1 = Jogo('Tetris', 'Puzzle', 'Atari')
 jogo2 = Jogo('God Of War', 'Rack and Slash', 'PS2')
@@ -27,6 +28,25 @@ def criar():
     console = request.form['console']
     jogo = Jogo(nome, categoria, console)
     lista.append(jogo)
+    return redirect('/')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/autenticar', methods=['POST'])
+def autenticar():
+    if 'alohomora' == request.form['senha']:
+        session['usuario_logado'] = request.form['usuario']
+        flash(session['usuario_logado']+' logado com sucesso')
+        return redirect('/')
+    flash('Usuario nao logado.')
+    return redirect('/login')
+
+@app.route('/logout')
+def logout():
+    session['usuario_logado'] = None
+    flash('Logout efetuado com sucesso')
     return redirect('/')
 app.run(debug=True)
 
